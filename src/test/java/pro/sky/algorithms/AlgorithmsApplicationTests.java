e4a2f56dbed395785ae626b86c637a25a50ff3cc
Андрей <79538233369@ya.ru>
2024-06-12 18:52:03 +0500

    initial commit

---
+++ src/test/java/pro/sky/algorithms/AlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.algorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
